45312711e836bb446ef4485d9ecd8a24f5f5c1e6
= <=>
2021-10-20 22:47:40 +0200

    Problems added

---
+++ ProgramaMe_Problemas_C/src/dam/jorgeRe/y2021/navidad/p580_FotoMafalda/FotoMafalda.c
@@ -0,0 +1,20 @@
+/**
+ * ================
+ * Ejercicio: 580 fotoMafalda
+ * Author: Jkutkut
+ * ================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int cases, i;
+
+    scanf("%d", cases);
+
+    for (i = 0; i < cases; i++) {
+
+    }
+
+    return 0;
+}
