fc514493c254683e811ca4ce4c844a988e65bba9
= <=>
2021-11-07 23:52:46 +0100

    ready to start adding templates

--- ProgramaMe_Problemas/.settings/createProblem/template/getTemplate.sh
+++ ProgramaMe_Problemas/.settings/createProblem/template/getTemplate.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
+# Constants
+SETTINGS_FILE=".settings/settings.json";
+
 # Get template type
 templateType=$1;
+
+# get username from json file settings
+username=$(jq -r '.author' $SETTINGS_FILE);
+
+# Switch template type: java or c
+case $templateType in
+    .java)
+        
+        ;;
+    .c)
+        
+        ;;
+    *)
+        echo "Invalid template type";
+        exit 1;
+        ;;
