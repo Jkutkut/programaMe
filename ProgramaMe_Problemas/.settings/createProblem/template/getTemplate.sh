4f5f07550c5600192d457f63c6a6c2e5c3b5e1bb
= <=>
2021-11-08 00:21:05 +0100

    working on templates

--- ProgramaMe_Problemas/.settings/createProblem/template/getTemplate.sh
+++ ProgramaMe_Problemas/.settings/createProblem/template/getTemplate.sh
@@ -2,6 +2,7 @@
 
 # Constants
 SETTINGS_FILE=".settings/settings.json";
+TEMPLATES_DIR=".settings/createProblem/template/";
 
 # Get template type
 templateType=$1;
@@ -12,12 +13,17 @@ username=$(jq -r '.author' $SETTINGS_FILE);
 # Switch template type: java or c
 case $templateType in
     .java)
-        
+        templateFile=$TEMPLATES_DIR"java_templates.json";
         ;;
     .c)
-        
+        templateFile=$TEMPLATES_DIR"c_templates.json";
         ;;
     *)
         echo "Invalid template type";
         exit 1;
         ;;
+esac
+
+# Get all ids from json array separated by comma
+templateIds=$(jq -r '.templates[] | .id' $templateFile);
+
